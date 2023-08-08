8df443aed88a9b91f7ffd251c93dde8bbafe7689
Atya M. Jarghoun <atya.tee37@gmail.com>
2023-08-09 02:52:52 +0300

    new code

--- routes/user.php
+++ routes/user.php
@@ -1,9 +1,28 @@
-<?php
+<?php 
 
 use Illuminate\Support\Facades\Route;
 
 
+Route :: prefix ('user') -> name ('user.') -> group (function () {
+   
+  Route :: get ( 'profile', function () {
+    return 'User profile';
+  }) -> name ('profile');
+ 
+  Route :: get ( 'courses', function () {
+    return 'User courses';
+  }) -> name ('courses');
 
-Route::get('url',function(){
-    return '';
-    });
+  Route ::get ( 'comments', function () {
+    return 'User comments';
+  }) -> name ('comments');
+
+  Route :: get ( 'posts', function () {
+    return 'User posts';
+  }) -> name('posts');
+
+
+});
+
+
+
--- routes/web.php
+++ routes/web.php
@@ -2,47 +2,107 @@
 
 use Illuminate\Support\Facades\Route;
 
+// Route::get('/',function(){
+//  return 'homepage';
+// });
+
+// //Route::method('url','action')
+
+// Route::get('url',function(){
+// return '';
+// });
+
+// Route::post('url',function(){
+//     return '';
+//     });
+
+// Route::put('url',function(){
+//         return '';
+//         });
+
+
+// Route::patch('url',function(){
+//     return '';
+//     });
+
+// Route::delete('url',function(){
+//     return '';
+//     });
+
+// Route::options('url',function(){
+//     return '';
+//     });
+
+
+
+// Route::get('/',function(){
+//     return "";
+// });
+
+// Route::get('/about',function(){
+//     return "";
+// });
+
+// Route::get('/contact',function(){
+//     return "";
+// });
+
+// Route::get('',function(){
+// return '';
+// });
+
+// Route :: get ('',function(){
+//     return '';
+// });
+
+// Route :: get('',function(){
+//     return '';
+// });
+
+// Route :: get ('',function(){
+//     return '';
+// });
+
+// Route :: get ('',function(){
+//     return '';
+// });
+
+// Route :: get ('',function(){
+//     return '';
+
+// }); 
+
+// Route :: get ( '/',function(){
+//     return 'homepage';
+// });
+
+// index, about, contact, services, our team , blog 
+
 Route::get('/',function(){
- return 'homepage';
+    $link = route('about');
+    return "<a href = '$link'> About Us </a>";
 });
 
-//Route::method('url','action')
+Route::get('/aboutssssss',function(){
+    return'About Us ';
+}) -> name('about');
 
-Route::get('url',function(){
-return '';
-});
+Route::get('/course/{name}/{hours?}',function($name, $hours=0) {
 
-Route::post('url',function(){
-    return '';
-    });
+    if ($hours ==0 ) {
 
-Route::put('url',function(){
-        return '';
-        });
+        return "Course Name Is : $name";
 
+    }else {
 
-Route::patch('url',function(){
-    return '';
-    });
+        return "Course Name Is : $name & Course Hours = $hours" ;
 
-Route::delete('url',function(){
-    return '';
-    });
+    }
 
-Route::options('url',function(){
-    return '';
-    });
+    
+})->name('course.single ');
 
 
-
-Route::get('/',function(){
-    return "";
-});
-
-Route::get('/about',function(){
-    return "";
-});
-
-Route::get('/contact',function(){
-    return "";
-});
+Route::get('subject/{name}',function($name){
+    return "Subject $name";
+}) -> whereAlpha('name');
